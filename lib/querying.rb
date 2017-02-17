1254e47b0f3b90398375d394d2c70d795626b35b
Mariel Segovia <msegovia94@gmail.com>
2017-02-17 00:56:46 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -2,7 +2,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT title, year
   FROM books
   WHERE series_id = 1
-  ORDER BY year ASC;"
+  ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
@@ -23,26 +23,26 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors
-  INNER JOIN series ON authors.id = series.author_id
-  INNER JOIN subgenres ON series.subgenre_id = subgenres.id;"
+  JOIN series ON authors.id = series.author_id
+  JOIN subgenres ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title
-  FROM characters
-  INNER JOIN series
-  ON characters.series_id = series.id
+  FROM series
+  JOIN books ON books.series_id = series.id
+  JOIN character_books ON character_books.book_id = books.id
+  JOIN characters ON character_books.character_id = characters.id
   WHERE characters.species = 'human'
   GROUP BY series.title
   ORDER BY COUNT(*) DESC
-  LIMIT 1;
-"
+  LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(*)
+  "SELECT characters.name, COUNT(*) as book_count
   FROM character_books
-  INNER JOIN characters ON characters.id = character_books.character_id
+  JOIN characters ON characters.id = character_books.character_id
   GROUP BY characters.name
-  ORDER BY COUNT(characters.name) DESC;"
+  ORDER BY book_count DESC;"
 end
